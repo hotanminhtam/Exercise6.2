e52270430fb32e622bec0e79908b4f13ec065320
hungdp2612 <hungdp2612@gmail.com>
2019-10-24 17:43:07 +0700

    ok

---
+++ Exercise6.2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise6._2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPush()
+        {
+            STACK s = new STACK();
+            s.push(1);
+            s.push(2);
+            s.push(3);
+            Assert.AreEqual(3, s.pop());
+            Assert.AreEqual(2, s.pop());
+        }
+    }
+}
